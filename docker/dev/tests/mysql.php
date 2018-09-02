d0db1562daa18e4becae3f527f01db1f7a4564f1
yorick <millband_paul@hotmail.co.uk>
2018-09-02 12:40:49 +0100

    better mysql test readout

--- docker/dev/tests/mysql.php
+++ docker/dev/tests/mysql.php
@@ -3,11 +3,25 @@ $servername = "db";
 $username = "devuser";
 $password = "devpass";
 
-// Create connection
+
+
+ // Create connection
 $conn = new mysqli($servername, $username, $password);
 
 // Check connection
 if ($conn->connect_error) {
     die("Connection failed: \n" . $conn->connect_error);
 }
-echo "Connected successfully \n";
+echo "Connected successfully \n<br><br>";
+echo '$servername = '.$servername.'<br>';
+echo '$username = '.$username.'<br>';
+echo '$password = '.$password.'<br>';
+echo <<<EOF
+    <br>
+    databases found:-<br>
+EOF;
+
+$result = mysqli_query($conn,"SHOW DATABASES");
+while ($row = mysqli_fetch_array($result)) {
+    echo $row[0]."<br>";
+}
