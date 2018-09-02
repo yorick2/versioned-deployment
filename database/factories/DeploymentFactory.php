71ce9725db196a9f3457906a372f21ec84ab4b43
yorick <millband_paul@hotmail.co.uk>
2018-09-02 17:03:24 +0100

    add deployment factory

---
+++ database/factories/DeploymentFactory.php
@@ -0,0 +1,17 @@
+<?php
+
+use Faker\Generator as Faker;
+
+$factory->define(App\Deployment::class, function (Faker $faker) {
+    return [
+        'server_id' => function(){
+            return factory('App\Server')->create()->id;
+        },
+        'user_id' => function(){
+            return factory('App\User')->create()->id;
+        },
+        'success' => $faker->boolean,
+        'notes' => $faker->paragraph,
+        'output' => $faker->paragraph
+    ];
+});
