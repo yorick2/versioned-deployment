80c036dd4b7d8fbf876390ee72b77dae432e4f9a
yorick <millband_paul@hotmail.co.uk>
2018-11-29 13:42:18 +0000

    update navbar

--- resources/views/layouts/nav.blade.php
+++ resources/views/layouts/nav.blade.php
@@ -11,7 +11,7 @@
             <!-- Left Side Of Navbar -->
             <ul class="navbar-nav mr-auto">
                 <li class="nav-item">
-                    <a href="{{ URL::previous() }}" class="btn btn-default">Back</a>
+                    <a href="{{ URL::previous() }}" class="btn btn-default">&#8810; Back</a>
                 </li>
             </ul>
 
@@ -26,6 +26,9 @@
                         <a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a>
                     </li>
                 @else
+                    <li class="nav-item">
+                        <a href="{{ url('/projects') }}" class="btn btn-default">Projects</a>
+                    </li>
                     <li class="nav-item dropdown">
                         <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
                             {{ Auth::user()->name }} <span class="caret"></span>
