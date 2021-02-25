a92561757d0e3f3f6249c9082cacaaf0153de8e6
Paul Millband <millband_paul@hotmail.co.uk>
2021-02-25 17:30:28 +0000

    docker deploy bug fixs

--- docker/dev/setup_test_laravel.sh
+++ docker/dev/setup_test_laravel.sh
@@ -9,7 +9,8 @@ chgrp -R www-data /var/www/html/storage /var/www/html/bootstrap/cache
 chmod -R ug+w /var/www/html/storage /var/www/html/bootstrap/cache /var/www/html/database
 composer install --no-interaction --working-dir=/var/www/html
 
-if [[ $(php artisan migrate:status) = *"No migrations found"* ]]; then
+if [[ $(php artisan migrate:status) = *"No migrations found"* ]] || [[ $(php artisan migrate:status) = *"Undefined index"* ]]; then
+    echo "runnning migration"
     php /var/www/html/artisan clear-compiled
     php /var/www/html/artisan optimize
     php /var/www/html/artisan key:generate
@@ -17,6 +18,9 @@ if [[ $(php artisan migrate:status) = *"No migrations found"* ]]; then
     php /var/www/html/artisan migrate
     php /var/www/html/artisan db:seed
 fi
+
+# ensure no unusual log files are created owned by root
+chgrp -R www-data /var/www/html/storage 
 
 echo '
 ███████╗██╗████████╗███████╗    ██████╗ ███████╗ █████╗ ██████╗ ██╗   ██╗
